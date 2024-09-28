4997b6c7ba4874187f4303f38f7c6b11c0fdce03
manuruizb <ruizbedoyamanuela@gmail.com>
2024-09-28 17:23:51 -0500

    prueba

--- entorno/Scripts/activate.bat
+++ entorno/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Documentos\Repositorios-Manu\TendenciasDesarrollo\proyecto_clinica_grupo2\clinica\entorno
+set VIRTUAL_ENV=C:\Documentos\Repositorios-Manu\TendenciasDesarrollo\proyecto_clinica_grupo2\entorno
 
 if not defined PROMPT set PROMPT=$P$G
 
